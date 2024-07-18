33f12b7597874675a890c0f7e36d051ce379c08f
Teddy Le <teddyle257@gmail.com>
2024-07-18 10:33:59 +0200

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+let BACKEND_URL = 'http://localhost:3000';
+
+
+if (BACKEND_URL[BACKEND_URL.length - 1] === '/') {
+  BACKEND_URL = BACKEND_URL.slice(0, -1);
+}  
+
+setInterval(function () {
+  fetch(`${BACKEND_URL}/time`)
+    .then(response => response.json())
+    .then(data => {
+      const timer = document.getElementById('timer');
+      timer.innerHTML = data.time;
+    });
+}, 1000);
